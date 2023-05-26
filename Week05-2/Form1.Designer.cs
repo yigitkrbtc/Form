e5363802e81b2432c782a728f05b8f94ed071b6c
yigitkrbtc <112809652+yigitkrbtc@users.noreply.github.com>
2023-05-26 20:52:40 +0300

    Add files via upload

---
+++ Week05-2/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Week05_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnLastMessage = new System.Windows.Forms.Button();
+            this.lblLastMessage = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSignUp);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtFullName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Personal Information";
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Location = new System.Drawing.Point(189, 66);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(75, 23);
+            this.btnSignUp.TabIndex = 4;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(86, 40);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(178, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email Address";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(86, 17);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(178, 20);
+            this.txtFullName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSend);
+            this.groupBox2.Controls.Add(this.txtMessage);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(270, 146);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Message";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(189, 117);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(10, 20);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(254, 91);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(12, 267);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(41, 15);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "label1";
+            // 
+            // btnLastMessage
+            // 
+            this.btnLastMessage.Location = new System.Drawing.Point(313, 12);
+            this.btnLastMessage.Name = "btnLastMessage";
+            this.btnLastMessage.Size = new System.Drawing.Size(177, 23);
+            this.btnLastMessage.TabIndex = 3;
+            this.btnLastMessage.Text = "Get Last Message";
+            this.btnLastMessage.UseVisualStyleBackColor = true;
+            this.btnLastMessage.Click += new System.EventHandler(this.btnLastMessage_Click);
+            // 
+            // lblLastMessage
+            // 
+            this.lblLastMessage.AutoSize = true;
+            this.lblLastMessage.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblLastMessage.Location = new System.Drawing.Point(310, 52);
+            this.lblLastMessage.Name = "lblLastMessage";
+            this.lblLastMessage.Size = new System.Drawing.Size(56, 17);
+            this.lblLastMessage.TabIndex = 4;
+            this.lblLastMessage.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 320);
+            this.Controls.Add(this.lblLastMessage);
+            this.Controls.Add(this.btnLastMessage);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Message Sender Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnLastMessage;
+        private System.Windows.Forms.Label lblLastMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Week05-2/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week05_2
+{
+    public partial class Form1 : Form
+    {
+        public string fullname { get; set; }
+        public string email { get; set; }
+        public string message { get; set; }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtFullName.Text.Trim()))
+                    throw new Exception("Please enter your full name");
+                if (txtFullName.Text.Trim().Split(' ').Length < 2)
+                    throw new Exception("Please enter your name and surname");
+
+                if (string.IsNullOrEmpty(txtEmail.Text.Trim()))
+                    throw new Exception("Please enter your email");
+                if (txtEmail.Text.Trim().Split('@').Length < 2)
+                    throw new Exception("Please enter a valid email");
+
+                this.fullname = txtFullName.Text.Trim();
+                this.email = txtEmail.Text.Trim();
+
+                MessageBox.Show("Sign up successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }    
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtMessage.Text.Trim()))
+                    throw new Exception("Please enter your message");
+                if(string.IsNullOrEmpty(this.fullname) || string.IsNullOrEmpty(this.email))
+                    throw new Exception("Please sign up to send message");
+
+                this.message = txtMessage.Text.Trim();
+
+                timer2.Start();
+                MessageBox.Show("Message sent successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnLastMessage_Click(object sender, EventArgs e)
+        {
+            displayLastMessage();
+        }
+
+        private void displayLastMessage()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("Your Name: " + this.fullname);
+            builder.AppendLine("Email    : " + this.email);
+            builder.AppendLine("Message  :");
+            builder.AppendLine(this.message);
+
+            lblLastMessage.Text = builder.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtFullName.Text = "";
+            txtEmail.Text = "";
+            txtMessage.Text = "";
+
+            lblTime.Text = string.Empty;
+            lblLastMessage.Text = string.Empty;
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            displayTheTime();
+        }
+
+        private void displayTheTime()
+        {
+            lblTime.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            displayLastMessage();
+            timer2.Stop();
+        }
+
+        private void linkGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(linkGithub.Text);
+        }
+    }
+}
